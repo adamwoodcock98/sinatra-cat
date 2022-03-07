84a85df6b334cca60d5f6b19313c28d270c9db79
Nicola Cortese <nicolacortese92@gmail.com>
2022-03-07 16:43:01 +0000

    saved our arguements to our params and then passed them to our instance variable

--- app.rb
+++ app.rb
@@ -9,8 +9,13 @@ get '/secret' do
   "adam keeps a blog"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Lats", "Misty", "Almond"].sample
   erb :index
-  
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
+  erb :index
 end
